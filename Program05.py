742d35bb0990c1e24599350e7ecc09954af96283
toyib32 <toyibhidayat32@gmail.com>
2020-10-21 00:12:06 +0700

    Create Program05.py

---
+++ Program05.py
@@ -0,0 +1,9 @@
+mhs_Laki2 = input("Mahasiswa Laki-Laki:")
+mhs_Perempuan = input("Mahasiswa Perempuan:")
+
+# cetak diagram
+diagram_Laki2 = int(mhs_Laki2)//10
+diagram_Perempuan = int(mhs_Perempuan)//10
+
+print("Laki_laki :","*" * diagram_Laki2, int(mhs_Laki2))
+print("Perempuan :","*" * diagram_Perempuan, int(mhs_Perempuan))
