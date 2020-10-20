b365fd6c67f2dfcc1e934312be3ad4fb27dcc4d6
toyib32 <toyibhidayat32@gmail.com>
2020-10-21 00:11:52 +0700

    Create Program02.py

---
+++ Program02.py
@@ -0,0 +1,9 @@
+totalTempuh = 795
+jarakPerLiter = 12
+efisiensiBbm = 1
+
+# hitung konsumsi bbm
+konsumsiBbmTotal = float(totalTempuh/(jarakPerLiter/efisiensiBbm))
+print("maka bahan bakar yang dibutuhkan adalah", (konsumsiBbmTotal), "liter")
+
+
